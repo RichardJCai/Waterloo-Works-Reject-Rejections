f74f0d67a958b3d4c3b80c7d40db893f21355f54
Richard Cai <RichardJCai@users.noreply.github.com>
2017-02-10 13:59:41 -0500

    Minor Changes
    The extension now changes the number of postings and displays interviews you were selected for.

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
+function changeDisplay(counter){
+	var display = document.getElementsByClassName("badge badge-info");
+	var total;
+	for (var i = 0; i < display.length;i++ ){
+		total = display[i].innerHTML;
+		display[i].innerHTML = (parseInt(total) - counter);
+	}
+	
+	var display2 = document.getElementById("totalOverAllPacks") ;
+	display2.innerHTML = parseInt(total) - counter;
+}
+
 function remove() {
     var table = document.getElementById("postingsTable");
+	var counter = 0;
     for (var i = 0, row; row = table.rows[i]; i++) {
         for (var j = 0, col; col = row.cells[j]; j++) {
-               if(table.rows[i].cells[j].innerHTML.includes("Filled")){
+				if(table.rows[i].cells[j].innerHTML.includes("Selected for Interview")){
+                  break;
+               }
+               else if(table.rows[i].cells[j].innerHTML.includes("Filled")){
                   table.deleteRow(i);
                   i--;
+				  counter++;
                   break;
                }
                else if(table.rows[i].cells[j].innerHTML.includes("Emp Rankings Finalized")){
                  table.deleteRow(i);
                  i--;
+				 counter++;
                  break;
                }
                else if(table.rows[i].cells[j].innerHTML.includes("Interview Complete")){
                  table.deleteRow(i);
                  i--;
+				 counter++;
                  break;
                }
                else if(table.rows[i].cells[j].innerHTML.includes("Interview Selections Complete")){
                  table.deleteRow(i);
                  i--;
+				 counter++;
+                 break;
+               }
+			   else if(table.rows[i].cells[j].innerHTML.includes("Cancel")){
+                 table.deleteRow(i);
+                 i--;
+				 counter++;
                  break;
                   }
                }
         }
+		changeDisplay(counter);
     }
+	
 
 
 var ul = document.getElementsByClassName("childMenu1 nav nav-list");
